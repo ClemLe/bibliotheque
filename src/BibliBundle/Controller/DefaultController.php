981e91f5c47cb76632a6c4e82fa147798850f8af
Clement <Clement@Clement-PC>
2016-11-07 21:55:46 +0100

    commit create bundle

---
+++ src/BibliBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BibliBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BibliBundle:Default:index.html.twig');
+    }
+}
