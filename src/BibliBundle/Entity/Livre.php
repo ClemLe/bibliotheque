ba4fd8a58d7dc333579d1a731695735fb49c94d6
olivier <>
2016-11-14 22:20:01 +0100

    generation de la table liste

--- src/BibliBundle/Entity/Livre.php
+++ src/BibliBundle/Entity/Livre.php
@@ -8,10 +8,11 @@ use Doctrine\ORM\Mapping as ORM;
  * Livre
  *
  * @ORM\Table(name="livre")
+ * @ORM\Entity(repositoryClass="BibliBundle\Repository\LivreRepository")
  */
-class Livre{
-    
-     /**
+class Livre
+{
+    /**
      * @var int
      *
      * @ORM\Column(name="id", type="integer")
@@ -19,56 +20,225 @@ class Livre{
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-    
-     /**
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="title", type="string", length=255)
+     * @ORM\Column(name="titre", type="string", length=255)
      */
     private $titre;
-    
-     /**
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="author", type="string", length=255)
+     * @ORM\Column(name="auteur", type="string", length=255)
      */
-    
     private $auteur;
-    
-        
-     /**
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="title", type="string", length=255)
+     * @ORM\Column(name="resume", type="text")
      */
     private $resume;
-    
-     /**
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="title", type="string", length=255)
+     * @ORM\Column(name="couverture", type="string", length=255)
      */
     private $couverture;
-    
-     /**
+
+    /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="datetime")
+     * @ORM\Column(name="dateEmprunt", type="datetime")
      */
     private $dateEmprunt;
-    
+
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="datetime")
+     * @ORM\Column(name="dateReservation", type="datetime")
      */
     private $dateReservation;
-    
+
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="datetime")
+     * @ORM\Column(name="dateRetour", type="datetime")
      */
     private $dateRetour;
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     * @return Livre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     * @return Livre
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string 
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * Set resume
+     *
+     * @param string $resume
+     * @return Livre
+     */
+    public function setResume($resume)
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    /**
+     * Get resume
+     *
+     * @return string 
+     */
+    public function getResume()
+    {
+        return $this->resume;
+    }
+
+    /**
+     * Set couverture
+     *
+     * @param string $couverture
+     * @return Livre
+     */
+    public function setCouverture($couverture)
+    {
+        $this->couverture = $couverture;
+
+        return $this;
+    }
+
+    /**
+     * Get couverture
+     *
+     * @return string 
+     */
+    public function getCouverture()
+    {
+        return $this->couverture;
+    }
+
+    /**
+     * Set dateEmprunt
+     *
+     * @param \DateTime $dateEmprunt
+     * @return Livre
+     */
+    public function setDateEmprunt($dateEmprunt)
+    {
+        $this->dateEmprunt = $dateEmprunt;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEmprunt
+     *
+     * @return \DateTime 
+     */
+    public function getDateEmprunt()
+    {
+        return $this->dateEmprunt;
+    }
+
+    /**
+     * Set dateReservation
+     *
+     * @param \DateTime $dateReservation
+     * @return Livre
+     */
+    public function setDateReservation($dateReservation)
+    {
+        $this->dateReservation = $dateReservation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateReservation
+     *
+     * @return \DateTime 
+     */
+    public function getDateReservation()
+    {
+        return $this->dateReservation;
+    }
+
+    /**
+     * Set dateRetour
+     *
+     * @param \DateTime $dateRetour
+     * @return Livre
+     */
+    public function setDateRetour($dateRetour)
+    {
+        $this->dateRetour = $dateRetour;
+
+        return $this;
+    }
+
+    /**
+     * Get dateRetour
+     *
+     * @return \DateTime 
+     */
+    public function getDateRetour()
+    {
+        return $this->dateRetour;
+    }
 }
