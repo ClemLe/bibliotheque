db4e32692357e899439a870043411ecb58f12f1f
Clement <Clement@Clement-PC>
2016-11-20 13:46:36 +0100

    commit envoie d'un message

--- src/BibliBundle/Controller/ContactController.php
+++ src/BibliBundle/Controller/ContactController.php
@@ -3,11 +3,34 @@
 namespace BibliBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BibliBundle\Entity\Message;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
-    public function indexContactAction()
-    {
+    public function indexContactAction(Request $request)
+    {   
+        if (isset($_POST['action'])){
+            
+            $name=mysql_real_escape_string($_POST['name']);
+            $subject=mysql_real_escape_string($_POST['subject']);
+            $message=mysql_real_escape_string($_POST['message']);
+            $email=mysql_real_escape_string($_POST['email']);
+            
+            $message= new Message();
+            $message->setAuthorname($name);
+            $message->setEmail($email);
+            $message->setSubject($subject);
+            $message->setMessage($message);
+            $message->setIsRead(false);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+        }
+        
+                
+        
         return $this->render('BibliBundle:Contact:index.html.twig');
     }
 }
---
+++ src/BibliBundle/Entity/Message.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace BibliBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="BibliBundle\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="authorname", type="string", length=255)
+     */
+    private $authorname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subject", type="string", length=255)
+     */
+    private $subject;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+    
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isRead", type="boolean")
+     */
+    private $isRead;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set authorname
+     *
+     * @param string $authorname
+     * @return Message
+     */
+    public function setAuthorname($authorname)
+    {
+        $this->authorname = $authorname;
+
+        return $this;
+    }
+
+    /**
+     * Get authorname
+     *
+     * @return string 
+     */
+    public function getAuthorname()
+    {
+        return $this->authorname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Message
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set subject
+     *
+     * @param string $subject
+     * @return Message
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get subject
+     *
+     * @return string 
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set isRead
+     *
+     * @param boolean $isRead
+     * @return Message
+     */
+    public function setIsRead($isRead)
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    /**
+     * Get isRead
+     *
+     * @return boolean 
+     */
+    public function getIsRead()
+    {
+        return $this->isRead;
+    }
+}
