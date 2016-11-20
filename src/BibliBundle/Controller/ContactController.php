b4df914714453cb3b54b84d8957535e074f399c3
Clement <Clement@Clement-PC>
2016-11-20 17:11:47 +0100

    commit modification envoie d'un message

--- src/BibliBundle/Controller/ContactController.php
+++ src/BibliBundle/Controller/ContactController.php
@@ -10,27 +10,33 @@ class ContactController extends Controller
 {
     public function indexContactAction(Request $request)
     {   
+        
+        $codeErreur=null;
         if (isset($_POST['action'])){
             
             $name=mysql_real_escape_string($_POST['name']);
             $subject=mysql_real_escape_string($_POST['subject']);
-            $message=mysql_real_escape_string($_POST['message']);
+            $messagebody=mysql_real_escape_string($_POST['message']);
             $email=mysql_real_escape_string($_POST['email']);
             
             $message= new Message();
             $message->setAuthorname($name);
             $message->setEmail($email);
             $message->setSubject($subject);
-            $message->setMessage($message);
+            $message->setMessage($messagebody);
             $message->setIsRead(false);
             
             $em = $this->getDoctrine()->getManager();
             $em->persist($message);
             $em->flush();
+            
+            $codeErreur=1;
         }
         
                 
         
-        return $this->render('BibliBundle:Contact:index.html.twig');
+        return $this->render('BibliBundle:Contact:index.html.twig',array(
+          'codeErreur' => $codeErreur,
+        ));
     }
 }
