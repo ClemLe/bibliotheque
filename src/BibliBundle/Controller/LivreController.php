78246a64e03a9b3da9dc8392845f31e02c82019d
olivier <>
2016-11-21 22:09:48 +0100

    add ajouter livre

---
+++ src/BibliBundle/Controller/LivreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BibliBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BibliBundle\Entity\Livre;
+use Symfony\Component\HttpFoundation\Request;
+
+class LivreController extends Controller
+{
+    public function ajouterAction(Request $request){   
+        $codeErreur=null;
+        if (isset($_POST['action'])){
+            
+            $title=mysql_real_escape_string($_POST['titre']);
+            $auteur=mysql_real_escape_string($_POST['auteur']);
+            $resume=mysql_real_escape_string($_POST['resume']);
+            
+            $livre= new Livre();
+            $livre->setTitre($title);
+            $livre->setAuteur($auteur);
+            $livre->setResume($resume);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($livre);
+            $em->flush();
+            
+            $codeErreur=1;
+        }         
+        
+        return $this->render('BibliBundle:AjouterLivre:index.html.twig',array(
+          'codeErreur' => $codeErreur,
+        ));
+    }
+}
