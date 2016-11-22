dc8534c1ab5d5124ddcd5633f82369c75d9eb238
Clement <Clement@Clement-PC>
2016-11-23 00:56:32 +0100

    commit ajout du rôle Employe + ajout de la consultation des messages par les employes

--- src/BibliBundle/Controller/ContactController.php
+++ src/BibliBundle/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace BibliBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use BibliBundle\Entity\Message;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
@@ -38,5 +39,57 @@ class ContactController extends Controller
         return $this->render('BibliBundle:Contact:index.html.twig',array(
           'codeErreur' => $codeErreur,
         ));
+    }
+    
+    
+    
+    public function consulterMessageAction(){
+        
+        $messages = $this->getDoctrine()
+        ->getRepository('BibliBundle:Message')
+        ->findAll();
+        
+        
+       return $this->render('BibliBundle:Contact:consulter.messages.html.twig',array(
+           'messages' => $messages
+       ));
+    }
+    
+    public function consulterUnMessageAction($idMessage){
+        
+        $message=$this->getDoctrine()->getRepository('BibliBundle:Message')->findOneBy(array('id' => $idMessage));
+        $message->setIsRead(true);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($message);
+        $em->flush();
+        
+        
+      
+       $corpsMessage=htmlentities($message->getMessage());
+        
+        
+        return $this->render('BibliBundle:Contact:consulter.un.message.html.twig',array(
+           'message' => $message,
+            'corpsMessage' => $corpsMessage
+
+        ));
+    }
+    
+    
+    public function calculNombreMessageAction(){
+        $nombreMessage=0;
+        $messages = $this->getDoctrine()
+        ->getRepository('BibliBundle:Message')
+        ->findAll();
+        
+        foreach ($messages as $message){
+            if($message->getIsRead() == false){
+                $nombreMessage++;
+            } 
+        }
+        
+        //echo json_encode($$nombreMessage);
+        
+        return new Response($nombreMessage);
     }
 }
