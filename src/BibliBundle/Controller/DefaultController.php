79870e2b59c7738e60cc6ef487e5b8d44f29d279
olivier <>
2017-01-10 20:45:29 +0100

    commentaires classe DefaultController

--- src/BibliBundle/Controller/DefaultController.php
+++ src/BibliBundle/Controller/DefaultController.php
@@ -4,8 +4,16 @@ namespace BibliBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Controlleur par défaut 
+ */
 class DefaultController extends Controller
 {
+    /**
+     * Retourne la vue par défaut du site
+     * 
+     * @return type La vue par défaut
+     */
     public function indexAction()
     {
         return $this->render('BibliBundle:Default:index.html.twig');
